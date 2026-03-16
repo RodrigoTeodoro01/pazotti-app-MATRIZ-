7d73ce0b839daeb85d767cd68405bdc162f52176
Rodrigo Teodoro <analises.vendaspazotti@gmail.com>
2026-03-16 07:32:17 -0300

    Update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pazotti-app/',
+  base: '/pazotti-app-MATRIZ-/',
 })
